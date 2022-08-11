1c543a17fd9e0d276d9cf1b9baff6a4e05652520
Ivan Jovic <100125296+IvanJovicTN@users.noreply.github.com>
2022-08-11 16:51:08 +0200

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+var postMsg = ''
+
+function postText(value){
+	console.log('iFrame sender: ' + value) 
+	parent.postMessage(value, "*")
+}
+
+
+const scriptsInEvents = {
+
+		async Game_events_Event72_Act11(runtime, localVars)
+		{
+			postText(runtime.globalVars.finalScore)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
