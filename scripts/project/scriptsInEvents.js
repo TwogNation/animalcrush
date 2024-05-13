f4528630c2f95c7cb6fa82e7621580cf092e2346
tanjounokamioku <matheusnaschaves@gmail.com>
2024-05-13 18:49:33 -0300

    changes from stage

--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -8,10 +8,77 @@ function postText(value){
 
 const scriptsInEvents = {
 
-		async Game_events_Event72_Act11(runtime, localVars)
-		{
-			postText(runtime.globalVars.finalScore)
+	async Game_events_Event2_Act1(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.onMessage = (event) => {
+			if (event.data.startsWith('s:')){
+				const score_ = event.data.split(':')[1];
+				console.log(score_);
+				runtime.globalVars.score = score_;
+				runtime.callFunction("updateScore")
+			}
 		}
+	},
+
+	async Game_events_Event51_Act3(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.send("s:5")
+	},
+
+	async Game_events_Event72_Act10(runtime, localVars)
+	{
+		runtime.globalVars.webSocket.close();
+	},
+
+	async Game_events_Event72_Act11(runtime, localVars)
+	{
+		window.parent.postMessage("WebSocketClosed", "*");
+	},
+
+	async Main_events_Event10_Act1(runtime, localVars)
+	{
+		const queryParams = new URLSearchParams(window.location.search)
+		const token = queryParams.get('token');
+		const gameId = queryParams.get('gameId');
+		console.log("clicked")
+					const textInstance = runtime.objects.ErrorText.getFirstInstance()
+					textInstance.text = "PLEASE WAIT..."
+					const button = runtime.objects.ButtonPlay.getFirstInstance();
+					button.destroy(); 
+		// Add a variable to track if the WebSocket is already connected or in the process of connecting
+		let isWebSocketConnectingOrConnected = false; 
+					
+		try {
+		    // Check if the WebSocket is already connected or in the process of connecting
+		    if (!isWebSocketConnectingOrConnected) {
+		        const webSocket = new WebSocket('wss://arcade.legacyarcade.com/ws', [token, gameId]);
+		        runtime.globalVars.webSocket = webSocket;
+		
+		        webSocket.onopen = (event) => {
+		            isWebSocketConnectingOrConnected = true;  // Set the flag to true when connection is established
+		            runtime.callFunction('startendless');
+		        };
+		
+		        webSocket.onclose = (event) => {
+		            isWebSocketConnectingOrConnected = false; // Reset the flag when the connection is closed
+		        };
+		
+		        webSocket.onerror = (event) => {
+		            isWebSocketConnectingOrConnected = false; // Reset the flag on error
+		        };
+		
+		        runtime.globalVars.playable = 1;
+		    }
+		} catch (e) {
+		    const textInstance = runtime.objects.ErrorText.getFirstInstance();
+		    textInstance.text = "ERROR CONNECTING";
+		    console.log("error connecting to server", e);
+		}
+		
+		
+		
+		
+	}
 
 };
 
